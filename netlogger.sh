e2aa9a3b41461d6d2c36df5fb9fd36239c912620
zeko9381 <41266882+zeko9381@users.noreply.github.com>
2020-12-06 23:38:40 +0100

    Add files via upload

---
+++ netlogger.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#Dnevnik o zbiranju porabe podatkov se zbira na en mesec
+dir=$(date +"%b-%Y")
+#nethogs je program za spremljanje omrežne aktivnosti po procesih oz. programih
+nethogs -d 60 -t eno1 &>> /home/zajc/.netlogger/$dir.log
+#-d 60: izpisuje vsake 60 sekund, da zmanjša velikost dnevnika 
+#-v 3: tretji način prikaza oz. prikaz v MB
+#-t: tracemode oz. način za parse-anje podatkov
+#eno1: kateri omrežni vmesnik naj spremlja
+#&>>: preusmeri stdout in stderr in append-a v datoteko
