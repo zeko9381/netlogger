c2d19546f4d789ab1274f41eaf0b2bf0edad0b1f
zeko9381 <41266882+zeko9381@users.noreply.github.com>
2020-12-07 00:43:18 +0100

    Update netlogger.sh

--- netlogger.sh
+++ netlogger.sh
@@ -2,7 +2,7 @@
 #Dnevnik o zbiranju porabe podatkov se zbira na en mesec
 dir=$(date +"%b-%Y")
 #nethogs je program za spremljanje omrežne aktivnosti po procesih oz. programih
-nethogs -d 60 -t eno1 &>> /home/zajc/.netlogger/$dir.log
+nethogs -d 60 -t eno1 &>> /var/log/netlogger/$dir.log
 #-d 60: izpisuje vsake 60 sekund, da zmanjša velikost dnevnika 
 #-v 3: tretji način prikaza oz. prikaz v MB
 #-t: tracemode oz. način za parse-anje podatkov
