4d8ef1e74020ea1388ac2b73903368967cb311fe
zeko9381 <41266882+zeko9381@users.noreply.github.com>
2020-12-07 18:19:55 +0100

    Adjusted permissions

--- netlogger_installer.sh
+++ netlogger_installer.sh
@@ -13,6 +13,7 @@ cp nethogs-parser/hogs.go /usr/lib/nethogs-parser/
 printf "Installing the Netlogger script...\n"
 mkdir -p /opt/netlogger
 cp netlogger /opt/netlogger/
+chmod +x /opt/netlogger/netlogger
 
 printf "Installing the systemd service and enabling it to run at startup...\n"
 cp netlogger.service /etc/systemd/system/
@@ -22,6 +23,7 @@ systemctl start netlogger.service
 
 printf "Installing the netlogger-read script...\n"
 cp netlogger-read /usr/bin/
+chmod +x /usr/bin/netlogger-read
 
 printf "Creating log directory...\n"
 mkdir -p /var/log/netlogger/
