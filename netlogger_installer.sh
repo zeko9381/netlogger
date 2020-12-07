4b75f8207a247d2ca302f6288ea9639e4f6b8690
zeko9381 <41266882+zeko9381@users.noreply.github.com>
2020-12-07 17:45:26 +0100

    Added an installer

---
+++ netlogger_installer.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+printf "Make sure to run this script as sudo!\n\n"
+
+printf "Downloading from repository...\n"
+git clone https://github.com/zeko9381/netlogger
+cd netlogger
+
+printf "Installing nethogs-parser...\n"
+mkdir /usr/lib/nethogs-parser
+cp nethogs-parser/hogs.go /usr/lib/nethogs-parser/
+
+printf "Installing the Netlogger script...\n"
+mkdir /opt/netlogger
+cp netlogger /opt/netlogger/
+
+printf "Installing the systemd service and enabling it to run at startup...\n"
+cp netlogger.service /etc/systemd/system/
+systemctl daemon-reload
+systemctl enable netlogger.service
+systemctl start netlogger.service
+
+printf "Installing the netlogger-read script...\n"
+cp netlogger-read /usr/bin/
+
+printf "Cleaning up...\n"
+rm -r ../netlogger
